2ba0a34a7eb10d94e28afe66fa89830460d24a48
shinnn <dminor.sw@gmail.com>
2014-01-01 03:21:09 +0900

    update to 0.2.0

--- bin/cli.js
+++ bin/cli.js
@@ -6,7 +6,7 @@ var program = require('commander');
 var ansi = require('ansi-styles');
 
 program
-	.version('0.1.1')
+	.version('0.2.0')
   .usage('[characters (5 or more and 7 or less)] [options]')
 	.option('-i, --id <tracking ID>', 'change tracking-ID')
 	.option('-d, --domain-name <domain>', 'change domain')
